88bc919e3b6cadace484612931d9af388273b8aa
DaviJuvino <davijuvinosouza@gmail.com>
2025-02-16 12:49:26 -0300

    Add template

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
+import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+
+if (environment.production) {
+  enableProdMode();
 }
 
-bootstrapApplication(App);
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideAnimations()]
+}).catch((err) => console.error(err));
